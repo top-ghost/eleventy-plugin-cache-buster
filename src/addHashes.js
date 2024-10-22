45373a7e606ab447c70d3d687f9a169a07ea3d72
Casey Kolderup <casey@kolderup.org>
2024-10-21 23:14:32 -0700

    only add hash to link rel if it's a stylesheet

--- src/addHashes.js
+++ src/addHashes.js
@@ -1,61 +1,73 @@
-const parse5 = require('parse5');
-const merge = require('deepmerge');
+const parse5 = require("parse5");
+const merge = require("deepmerge");
 
-const replaceStrings = require('./replaceStrings');
-const findNodes = require('./findNodes.js');
-const appendQueryParameter = require('./appendQueryParameter.js');
-const nodeHelper = require('./nodeHelper.js');
-const createResourceHash = require('./createResourceHash.js');
+const replaceStrings = require("./replaceStrings");
+const findNodes = require("./findNodes.js");
+const appendQueryParameter = require("./appendQueryParameter.js");
+const nodeHelper = require("./nodeHelper.js");
+const createResourceHash = require("./createResourceHash.js");
 
 const parserOptions = {
-    // required to get the tags' start and end position
-    sourceCodeLocationInfo: true
+  // required to get the tags' start and end position
+  sourceCodeLocationInfo: true,
 };
 
 const defaultOptions = {
-    hashParameter: 'v',
-    sourceAttributes: {
-        link: 'href',
-        script: 'src'
-    },
-    createResourceHash
+  hashParameter: "v",
+  sourceAttributes: {
+    link: { rel: "stylesheet" },
+    script: "src",
+  },
+  createResourceHash,
 };
 
 module.exports = function (outputDir, options = defaultOptions) {
-    const {
-        hashParameter,
-        sourceAttributes,
-        createResourceHash
-    } = merge(defaultOptions, options);
+  const { hashParameter, sourceAttributes, createResourceHash } = merge(
+    defaultOptions,
+    options
+  );
 
-    const resourceTagNames = Object.keys(sourceAttributes);
-    const isResourceTag = (node) => resourceTagNames.includes(node.tagName);
-    const findResourceNodes = findNodes.bind(null, isResourceTag);
+  const resourceTagNames = Object.keys(sourceAttributes);
 
-    return function addContentHashes (content, target) {
-        // ignore empty contents
-        if (!content.trim().length) {
-            return content;
-        }
+  let isResourceTag = null;
+  if (
+    resourceTagNames.includes(node.tagName) &&
+    typeof sourceAttributes[node.tagName] === Object
+  ) {
+    isResourceTag = (node) =>
+      Array.from(sourceAttributes[node.tagName]).every(
+        (pair) => node.getAttribute(pair[0]) === pair[1]
+      );
+  } else {
+    isResourceTag = (node) => resourceTagNames.includes(node.tagName);
+  }
 
-        const document = parse5.parse(content, parserOptions);
+  const findResourceNodes = findNodes.bind(null, isResourceTag);
 
-        const resourceNodes = findResourceNodes(document)
-            .map((node) => nodeHelper({ sourceAttributes }, node))
-            .filter((node) => node.hasValidSource());
+  return function addContentHashes(content, target) {
+    // ignore empty contents
+    if (!content.trim().length) {
+      return content;
+    }
 
-        const replacements = resourceNodes.map(function (nodeHelper) {
-            const source = nodeHelper.getSource();
-            const hash = createResourceHash(outputDir, source, target);
-            nodeHelper.setSource(appendQueryParameter(source, hashParameter, hash));
+    const document = parse5.parse(content, parserOptions);
 
-            return {
-                start: nodeHelper.getStart(),
-                end: nodeHelper.getEnd(),
-                value: parse5.serialize({ childNodes: [nodeHelper.getNode()] })
-            };
-        });
+    const resourceNodes = findResourceNodes(document)
+      .map((node) => nodeHelper({ sourceAttributes }, node))
+      .filter((node) => node.hasValidSource());
 
-        return replaceStrings(content, replacements);
-    };
+    const replacements = resourceNodes.map(function (nodeHelper) {
+      const source = nodeHelper.getSource();
+      const hash = createResourceHash(outputDir, source, target);
+      nodeHelper.setSource(appendQueryParameter(source, hashParameter, hash));
+
+      return {
+        start: nodeHelper.getStart(),
+        end: nodeHelper.getEnd(),
+        value: parse5.serialize({ childNodes: [nodeHelper.getNode()] }),
+      };
+    });
+
+    return replaceStrings(content, replacements);
+  };
 };
